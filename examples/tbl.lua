ac20464223772f49a58a643e4af1e51f1c98c0c0
Haitham <hmmelsm@gmail.com>
2026-02-08 14:12:33 +0200

    docs: adjust tbl example

--- examples/tbl.lua
+++ examples/tbl.lua
@@ -1,15 +1,5 @@
 local tbl = require("mods.tbl")
-
-local function show_sorted(label, ls)
-  local out = {}
-  for i = 1, #ls do
-    out[i] = ls[i]
-  end
-  table.sort(out, function(a, b)
-    return tostring(a) < tostring(b)
-  end)
-  print(label, out[1], out[2], out[3])
-end
+local unpack = table.unpack or unpack
 
 local user = {
   profile = { name = "Ada", age = 36 },
@@ -17,29 +7,29 @@ local user = {
   tags = { "dev", "ops" },
 }
 
-print("get:", tbl.get(user, "profile", "name"))
-print("count:", tbl.count(user))
-print("isempty:", tbl.isempty({}))
+print("get      : " .. tbl.get(user, "profile", "name"))
+print("count    : " .. tbl.count(user))
+print("isempty  : " .. tostring(tbl.isempty({})))
 
 local copied = tbl.copy(user)
-print("copy name:", copied.profile.name)
+print("copy name: " .. copied.profile.name)
 
-local deep = tbl.deepcopy({ a = { b = 1 } })
-print("deepcopy:", deep.a.b)
+local deep = tbl.deepcopy({ a = { b = "nested" } })
+print("deepcopy : " .. deep.a.b)
 
 local filtered = tbl.filter({ a = 1, b = 2, c = 3 }, function(v)
   return v % 2 == 1
 end)
-print("filter:", filtered.a, filtered.c)
+print(("filter   : %s %s %s"):format(filtered.a, filtered.b, filtered.c))
 
 local inverted = tbl.invert({ a = 1, b = 2 })
-print("invert:", inverted[1], inverted[2])
+print(("invert   : %s %s"):format(inverted[1], inverted[2]))
 
 local keys = tbl.keys({ a = true, b = true, c = true })
-show_sorted("keys:", keys)
+print(("keys     : { %s, %s, %s }"):format(unpack(keys)))
 
 local values = tbl.values({ a = 10, b = 20 })
-show_sorted("values:", values)
+print(("values   : { %s, %s }"):format(unpack(values)))
 
 local merged = tbl.update({ a = 1 }, { b = 2 })
-print("update:", merged.a, merged.b)
+print(("update   : %s %s"):format(merged.a, merged.b))
