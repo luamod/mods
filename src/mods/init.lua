d7d6facd64bc691d6801beed149a3bf0c2d4e362
Haitham <hmmelsm@gmail.com>
2026-02-08 12:56:24 +0200

    feat: lazy-load mods in init

--- src/mods/init.lua
+++ src/mods/init.lua
@@ -1,7 +1,21 @@
-return {
-  List = require("mods.List"),
-  Set = require("mods.Set"),
-  str = require("mods.str"),
-  stringcase = require("mods.stringcase"),
-  tbl = require("mods.tbl"),
-}
+local mods = {}
+for _, name in ipairs({ "List", "Set", "str", "stringcase", "tbl" }) do
+  mods[name] = "mods." .. name
+end
+
+---@type mods
+---@diagnostic disable-next-line: missing-fields
+local M = {}
+
+setmetatable(M, {
+  __index = function(t, k)
+    local mod = mods[k]
+    if mod then
+      local v = require(mod)
+      rawset(t, k, v)
+      return v
+    end
+  end,
+})
+
+return M
