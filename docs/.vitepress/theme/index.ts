1b7717cac378b60c7ed867c7f221db0fb1d7b5d4
Haitham <hmmelsm@gmail.com>
2026-02-17 15:02:54 +0200

    docs(vitepress): update theme index import

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
-// .vitepress/theme/index.ts
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
+import "virtual:group-icons.css";
 import "./style.css";
 
 export default {
