826a2bcde3b1a76c12768d097f2150a087e6b5fd
Haitham <hmmelsm@gmail.com>
2026-02-18 22:55:12 +0200

    refactor(str,utils): move identifier checks to keyword module

--- src/mods/utils.lua
+++ src/mods/utils.lua
@@ -1,19 +1,8 @@
 local find = string.find
 local gsub = string.gsub
-local match = string.match
 
 ---@type mods.utils
 local M = {}
-
-local lua_keywords = {}
-([[ and break do else elseif end false for function goto if in local
-    nil not or repeat return then true until while ]]):gsub("%w+", function(w)
-  lua_keywords[w] = true
-end)
-
-function M.isidentifier(s)
-  return not lua_keywords[s] and match(s, "^[%a_][%w_]*$") ~= nil
-end
 
 function M.quote(v)
   if find(v, '"', 1, true) and not find(v, "'", 1, true) then
