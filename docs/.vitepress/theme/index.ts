b522a2604a702457650f36dd82b1075b6cb8edac
Haitham <hmmelsm@gmail.com>
2026-03-08 09:37:36 +0200

    chore(docs): remove tabs theme integration

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,5 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
-import { enhanceAppWithTabs } from "vitepress-plugin-tabs/client";
 import CopyOrDownloadAsMarkdownButtons from "vitepress-plugin-llms/vitepress-components/CopyOrDownloadAsMarkdownButtons.vue";
 import ModulesTable from "./components/ModulesTable.vue";
 import "virtual:group-icons.css";
@@ -9,7 +8,6 @@ import "./style.css";
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
-    enhanceAppWithTabs(app);
     app.component(
       "CopyOrDownloadAsMarkdownButtons",
       CopyOrDownloadAsMarkdownButtons,
